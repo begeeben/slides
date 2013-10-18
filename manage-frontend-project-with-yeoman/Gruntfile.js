afd9285723f548598bc0977980d950fa82d23f74
Yi-Fan Liao <begeeben@gmail.com>
2013-10-18 17:12:03 +0800

    Add build tasks.

--- manage-frontend-project-with-yeoman/Gruntfile.js
+++ manage-frontend-project-with-yeoman/Gruntfile.js
@@ -2,7 +2,9 @@
 'use strict';
 
 var LIVERELOAD_PORT = 35729;
-var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var lrSnippet = require('connect-livereload')({
+    port: LIVERELOAD_PORT
+});
 var mountFolder = function (connect, dir) {
     return connect.static(require('path').resolve(dir));
 };
@@ -35,7 +37,7 @@ module.exports = function (grunt) {
             },
             livereload: {
                 options: {
-                    middleware: function (connect) {
+                    middleware: function(connect) {
                         return [
                             lrSnippet,
                             mountFolder(connect, '.')
@@ -48,8 +50,99 @@ module.exports = function (grunt) {
             server: {
                 path: 'http://localhost:<%= connect.options.port %>'
             }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    // cwd: '',
+                    dest: 'dist',
+                    src: [
+                        '*.{ico,png,txt}',
+                        '.htaccess',
+                        'steps/*',
+                        // 'bower_components/**/*',
+                        // 'images/{,*/}*.{gif,webp}',
+                        // 'styles/fonts/*'
+                    ]
+                }, {
+                    expand: true,
+                    cwd: '.tmp/images',
+                    dest: 'dist/images',
+                    src: [
+                        'generated/*'
+                    ]
+                }]
+            }
+            // styles: {
+            //     expand: true,
+            //     cwd: '<%= yeoman.app %>/styles',
+            //     dest: '.tmp/styles/',
+            //     src: '{,*/}*.css'
+            // }
+        },
+        useminPrepare: {
+            html: 'index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+        usemin: {
+            html: ['dist/{,*/}*.html'],
+            css: ['dist/styles/{,*/}*.css'],
+            options: {
+                dirs: ['dist']
+            }
+        },
+        cssmin: {
+            // By default, your `index.html` <!-- Usemin Block --> will take care of
+            // minification. This option is pre-configured if you do not wish to use
+            // Usemin blocks.
+            // dist: {
+            //   files: {
+            //     'dist/styles/main.css': [
+            //       '.tmp/styles/{,*/}*.css',
+            //       'styles/{,*/}*.css'
+            //     ]
+            //   }
+            // }
+        },
+        htmlmin: {
+          dist: {
+            options: {
+              /*removeCommentsFromCDATA: true,
+              // https://github.com/yeoman/grunt-usemin/issues/44
+              //collapseWhitespace: true,
+              collapseBooleanAttributes: true,
+              removeAttributeQuotes: true,
+              removeRedundantAttributes: true,
+              useShortDoctype: true,
+              removeEmptyAttributes: true,
+              removeOptionalTags: true*/
+            },
+            files: [{
+              expand: true,
+              // cwd: '',
+              src: ['*.html', 'views/*.html'],
+              dest: 'dist'
+            }]
+          }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/scripts/scripts.js': [
+                        'dist/scripts/scripts.js'
+                    ]
+                }
+            }
         }
     });
 
     grunt.registerTask('server', ['connect:livereload', 'open', 'watch']);
+
+    grunt.registerTask('build', ['useminPrepare', 'htmlmin', 'concat', 'copy:dist', 'cssmin', 'uglify', 'usemin']);
+
+    grunt.registerTask('default', ['server', 'build']);
 };
